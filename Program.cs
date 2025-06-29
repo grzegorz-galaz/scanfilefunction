93099c2a342b932f1062834b1a2c8905108ff6ff
Grzegorz Gałązka <galaz.wsb2@outlook.com>
2025-06-29 19:34:10 +0200

    Aktualizacja kodu funkcji i konfiguracji

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Storage.Blobs;
 using ScanFileFunction.Services;
 
 var builder = FunctionsApplication.CreateBuilder(args);
@@ -11,6 +12,12 @@ builder.ConfigureFunctionsWebApplication();
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights()
-    .AddSingleton<ClamAvScanner>();
+    .AddSingleton<ClamAvScanner>()
+    .AddSingleton(x =>
+    {
+        var config = x.GetRequiredService<IConfiguration>();
+        var blobConnectionString = config["AzureWebJobsStorage"];
+        return new BlobServiceClient(blobConnectionString);
+    });
 
 builder.Build().Run();
