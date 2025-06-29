c4545c4069abdb6eee8d32c8c747ee4683755610
Grzegorz Gałązka <grzegorz.galazka@hotmail.com>
2025-06-29 21:40:50 +0200

    Usunięcie ostrzeżeń

--- Models/ScanResultEvent.cs
+++ Models/ScanResultEvent.cs
@@ -2,7 +2,7 @@ namespace ScanFileFunction.Models;
 
 public class ScanResultEvent
 {
-    public string FileName { get; set; } = string.Empty
+    public string FileName { get; set; } = string.Empty;
     public string Container { get; set; } = string.Empty;
     public string Status { get; set; } = string.Empty; // "clean" lub "infected"
 }
