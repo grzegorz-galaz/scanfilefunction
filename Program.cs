795561a2752075e4f30095fa8ff87ba55951da98
Grzegorz Gałązka <grzegorz.galazka@hotmail.com>
2025-06-30 23:56:08 +0200

    Poprawki

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureLogging(logging =>
     {
+        logging.ClearProviders();
+        logging.AddConsole();
         logging.SetMinimumLevel(LogLevel.Information);
     })
     .Build();
